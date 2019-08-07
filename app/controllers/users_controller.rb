54a0a89fa18c35720f70df8e40934b8284c81d75
Annalisa <arosegraz@gmail.com>
2019-08-06 23:38:41 -0700

    remove any attempts to U or D user/pass for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET: /users/5
   get "/users/:slug" do
     if logged_in?
       @user = User.find_by_slug(params[:slug])
@@ -37,21 +36,6 @@ class UsersController < ApplicationController
     else
       redirect to "/"
     end
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
   end
 
 end

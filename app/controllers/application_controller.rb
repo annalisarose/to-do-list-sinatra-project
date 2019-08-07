c0a7da8d0424a00b815eb56fd921234707d94386
Annalisa <arosegraz@gmail.com>
2019-08-06 21:54:08 -0700

    replaced route and redirect params of user id with user slug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
 
   get "/" do
     if logged_in?
-      redirect to "/users/#{current_user.id}"
+      redirect to "/users/#{current_user.slug}"
     else
       erb :welcome
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/new.html'
     else
-      redirect to "/users/#{current_user.id}"
+      redirect to "/users/#{current_user.slug}"
     end
   end
 
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
      user = User.new(:username => params[:username], :password => params[:password])
      user.save
      session[:user_id] = user.id
-     redirect to "/users/#{user.id}"
+     redirect to "/users/#{user.slug}"
    end
  end
 
@@ -23,16 +23,16 @@ class UsersController < ApplicationController
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to "/users/#{user.id}"
+      redirect to "/users/#{user.slug}"
     else
       redirect to "/"
     end
   end
 
   # GET: /users/5
-  get "/users/:id" do
+  get "/users/:slug" do
     if logged_in?
-      @user = User.find_by_id(params[:id])
+      @user = User.find_by_slug(params[:slug])
       erb :"/users/index.html"
     else
       redirect to "/"

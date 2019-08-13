feda4db1d611a428c86a2b35483c6f70b8611565
Annalisa <arosegraz@gmail.com>
2019-08-13 10:01:38 -0700

    move slugifiable methods to concerns folder and extend to classes User and List

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,9 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :checkitems
   #accepts_nested_attributes_for :checkitems
+
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  def slug
-    username.downcase.gsub(" ","-")
-  end
+  include Slugifiable::InstanceMethods
+  extend Slugifiable::ClassMethods
 
-  def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
-  end
 
 end
